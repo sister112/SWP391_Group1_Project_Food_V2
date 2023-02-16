54bd5d5a826ceb1422d4d8cffa021313923fff0b
your name <ngochieu1128@gmail.com>
2023-02-16 22:26:20 +0700

    Test of loadUserProfile

---
+++ src/test/java/dao/GetUserByAccountIDTest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package dao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tran Thi Ngoc Hieu CE161025
+ */
+public class GetUserByAccountIDTest {
+    UserDAO udao;
+    
+    public GetUserByAccountIDTest() {
+        udao = new UserDAO();
+    }
+
+    @Test
+    public void testGetUserByAccountID() {
+        assertNotNull(udao.getUserByAccountID(5));
+    }
+    
+     @Test
+    public void testGetUserByAccountID1() {
+        assertNull(udao.getUserByAccountID(5));
+    }
+    
+     @Test
+    public void testGetUserByAccountID2() {
+        assertEquals(udao.getUserByAccountID(5).getAccountName(), "Lê Văn G1");
+    }
+}
