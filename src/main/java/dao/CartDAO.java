4487e956beb5d12bc7d14074b94049ec65f696e4
NghiaHH28 <nghiahh28@gmail.com>
2023-02-17 08:24:12 +0700

    fix web.xml v1

--- src/main/java/dao/CartDAO.java
+++ src/main/java/dao/CartDAO.java
@@ -16,30 +16,31 @@ import model.Cart;
  * @author NghiaHHCE160343@fpt.edu.vn
  */
 public class CartDAO {
+
     Connection con = null; // connect to SQL server
     PreparedStatement ps = null; // move query from Netbeen to SQl
     ResultSet rs = null; // save result query
-    
-    /**    
+
     /**
+     * /**
      * This function get list product in cart
-     * 
-     * Function get accountID from parameter then use query into database to get 
+     *
+     * Function get accountID from parameter then use query into database to get
      * all product in table cart by accountID.
-     * 
+     *
      * @param accountID Account ID of user
      * @return list product in cart of user
      */
     public ArrayList<Cart> getListCartByAccountID(int accountID) {
         try {
-            String query = "SELECT C.*, P.ProductName, P.ProductLinkImage ,P.ProductPrice, P.ProductSalePercent FROM [CART] C JOIN [PRODUCT] P ON C.ProductID = P.ProductID \n" +
-                            "WHERE AccountID = ?"; // query select form database
+            String query = "SELECT C.*, P.ProductName, P.ProductLinkImage ,P.ProductPrice, P.ProductSalePercent FROM [CART] C JOIN [PRODUCT] P ON C.ProductID = P.ProductID \n"
+                    + "WHERE AccountID = ?"; // query select form database
             con = new DBContext().getConnection(); // open conect database
             ps = con.prepareStatement(query); // set account ID into query
             ps.setInt(1, accountID); // set account ID into query
             rs = ps.executeQuery(); // execute query
             ArrayList<Cart> listCart = new ArrayList<>(); // create list product in cart
-            while (rs.next()) {                
+            while (rs.next()) {
                 listCart.add(new Cart(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getString(5), rs.getString(6), rs.getInt(7), rs.getInt(8)));
                 // add new item into list product in cart
             } // end while
@@ -49,8 +50,8 @@ public class CartDAO {
         } // end try catch
         return null; // return null if not product into cart
     }
-    
-/**
+
+    /**
      * This function add product into cart from home page
      *
      * Function use accountID and productID to insert new cart with cart
@@ -145,9 +146,10 @@ public class CartDAO {
 
     /**
      * This function get quantity of a product in cart
-     * 
-     * This function use accountID and productID to select quantity of a product in cart
-     * 
+     *
+     * This function use accountID and productID to select quantity of a product
+     * in cart
+     *
      * @param accountID Account ID of user
      * @param productID ID of product
      * @return int quantity of a product in cart
@@ -171,24 +173,5 @@ public class CartDAO {
         } // end try catch
         return 0; // return 0 if can not select quantity of a product in cart
     }
-    
-    
-    
-    
-    
-     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
